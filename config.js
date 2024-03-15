272041039f527c3a1c6c389586fa054312e92e6a
GleyciFerreira <133020511+GleyciFerreira@users.noreply.github.com>
2024-03-14 21:56:38 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+//<script type="module">
+  // Import the functions you need from the SDKs you need
+  // import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD-zPqjxUM7pKioIUAB4qJbqOIy3TyrthU",
+    authDomain: "pw3etec-6eb77.firebaseapp.com",
+    databaseURL: "https://pw3etec-6eb77-default-rtdb.firebaseio.com",
+    projectId: "pw3etec-6eb77",
+    storageBucket: "pw3etec-6eb77.appspot.com",
+    messagingSenderId: "781105040946",
+    appId: "1:781105040946:web:85e86526dfbf4a9d0e762b"
+  };
+
+  // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    let auth = firebase.auth()
+
+
+  //</script>
